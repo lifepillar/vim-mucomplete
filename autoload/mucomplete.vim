lifepillar/vim-mucomplete

commit c173570ac8dd4a4046885408f5269c32e95d11fe

Fix problem with remappings.
Before this commit, there was a problem with completion mappings that
remapped to something else (like SQL <c-c> mappings), due to the fact
that in something like

    <c-r>="\<c-c>a"<cr>

`<c-c>a` is not expanded. So, for example, the following completion
chain was buggy:

    let g:mucomplete#user_mappings = {
        \ 'sqls' : ["\<c-c>s", "\<c-c>s\<c-p>"],
        \ 'sqlk' : ["\<c-c>k", "\<c-c>k\<c-p>"]
        \ }
    let g:mucomplete#chains = { 'sql' : ['file', 'sqls', 'sqlk', 'keyn'] }

When typing something like `ocr<tab> ta<tab>`, the first <tab> would
correctly complete the keyword `create` using <c-c>s, but the second
would fail. The second <tab> would be remapped to

    <c-c>s<c-r>=pumvisible()?'':mucomplete#complete_chain(1)<cr>

Since <c-c>s would not return any result, mucomplete#complete_chain(1)
would be called and <c-c>k fed to the buffer. But this latter mapping,
being evaluated inside <c-r>=, would not be remapped, causing Vim to
exit Insert mode and go up one line (because of `k`).

To fix that, I have introduced a new <plug>, to be used internally,
which is used by mucomplete#complete_chain() in place of the previous
recursive call.

autoload/mucomplete.vim
@@ -84,20 +84,23 @@ unlet s:cnp
 let s:compl_methods = []
 let s:compl_text = ''
 let s:auto = 0
+let s:i = -1
 
 " Workhorse function for chained completion. Do not call directly.
-fun! mucomplete#complete_chain(index)
-  let i = a:index
-  while i < len(s:compl_methods) &&
+fun! mucomplete#complete_chain()
+  if pumvisible()
+    return ''
+  endif
+  let s:i += 1
+  while s:i < len(s:compl_methods) &&
         \ !get(get(g:mucomplete#can_complete, getbufvar("%","&ft"), {}),
-        \          s:compl_methods[i],
-        \          get(g:mucomplete#can_complete['default'], s:compl_methods[i], s:yes_you_can)
+        \          s:compl_methods[s:i],
+        \          get(g:mucomplete#can_complete['default'], s:compl_methods[s:i], s:yes_you_can)
         \ )(s:compl_text)
-    let i += 1
+    let s:i += 1
   endwhile
-  if i < len(s:compl_methods)
-    return s:compl_mappings[s:compl_methods[i]][s:auto] .
-          \ "\<c-r>=pumvisible()?'':mucomplete#complete_chain(".(i+1).")\<cr>"
+  if s:i < len(s:compl_methods)
+    return s:compl_mappings[s:compl_methods[s:i]][s:auto] . "\<plug>(MUcompleteNxt)"
   endif
   return ''
 endf
@@ -107,13 +110,15 @@ fun! s:complete(rev)
   if a:rev
     let s:compl_methods = reverse(copy(s:compl_methods))
   endif
-  return mucomplete#complete_chain(0)
+
+  return mucomplete#complete_chain()
 endf
 
 fun! mucomplete#complete(rev)
   if pumvisible()
     return a:rev ? "\<c-p>" : "\<c-n>"
   endif
+  let s:i = -1
   let s:auto = exists('#MUcompleteAuto')
   let s:compl_text = matchstr(strpart(getline('.'), 0, col('.') - 1), '\S\+$')
   return strlen(s:compl_text) == 0

plugin/mucomplete.vim
@@ -18,6 +18,7 @@ if !exists(":MUcompleteAutoOff")
   command -nargs=0 MUcompleteAutoOff :call mucomplete#disable_auto()
 endif
 
+imap <expr> <silent> <plug>(MUcompleteNxt) mucomplete#complete_chain()
 imap <expr> <silent> <plug>(MUcompleteFwd) mucomplete#complete(0)
 imap <expr> <silent> <plug>(MUcompleteBwd) mucomplete#complete(1)
 inoremap    <silent> <plug>(MUcompleteTab) <tab>

commit 0663e90ac266da9230f389b81d52d967c202a1f6

Fix regression with manual completion.
Fix a bug introduced by the change in c173570ac8dd.

Apparently, the added keys are necessary for pumvisible() to return the
correct value.

autoload/mucomplete.vim
@@ -64,21 +64,21 @@ let g:mucomplete#exit_ctrlx_keys = "\<c-b>\<bs>"
 " Internal status
 let s:cnp = "\<c-x>".g:mucomplete#exit_ctrlx_keys
 let s:compl_mappings = extend({
-      \ 'c-n'     :  [s:cnp."\<c-n>", s:cnp."\<c-n>\<c-p>"],
-      \ 'c-p'     :  [s:cnp."\<c-p>", s:cnp."\<c-p>\<c-n>"],
-      \ 'cmd'     :  ["\<c-x>\<c-v>", "\<c-x>\<c-v>\<c-p>"],
-      \ 'defs'    :  ["\<c-x>\<c-d>", "\<c-x>\<c-d>\<c-p>"],
-      \ 'dict'    :  ["\<c-x>\<c-k>", "\<c-x>\<c-k>\<c-p>"],
-      \ 'file'    :  ["\<c-x>\<c-f>", "\<c-x>\<c-f>\<c-p>"],
-      \ 'incl'    :  ["\<c-x>\<c-i>", "\<c-x>\<c-i>\<c-p>"],
-      \ 'keyn'    :  ["\<c-x>\<c-n>", "\<c-x>\<c-n>\<c-p>"],
-      \ 'keyp'    :  ["\<c-x>\<c-p>", "\<c-x>\<c-p>\<c-n>"],
-      \ 'line'    :  ["\<c-x>\<c-l>", "\<c-x>\<c-l>\<c-p>"],
-      \ 'omni'    :  ["\<c-x>\<c-o>", "\<c-x>\<c-o>\<c-p>"],
-      \ 'spel'    :  ["\<c-x>s"     , "\<c-x>s\<c-p>"     ],
-      \ 'tags'    :  ["\<c-x>\<c-]>", "\<c-x>\<c-]>\<c-p>"],
-      \ 'thes'    :  ["\<c-x>\<c-t>", "\<c-x>\<c-t>\<c-p>"],
-      \ 'user'    :  ["\<c-x>\<c-u>", "\<c-x>\<c-u>\<c-p>"]
+      \ 'c-n'     :  [s:cnp."\<c-n>\<c-p>\<c-n>", s:cnp."\<c-n>\<c-p>"],
+      \ 'c-p'     :  [s:cnp."\<c-p>\<c-n>\<c-p>", s:cnp."\<c-p>\<c-n>"],
+      \ 'cmd'     :  ["\<c-x>\<c-v>\<c-p>\<c-n>", "\<c-x>\<c-v>\<c-p>"],
+      \ 'defs'    :  ["\<c-x>\<c-d>\<c-p>\<c-n>", "\<c-x>\<c-d>\<c-p>"],
+      \ 'dict'    :  ["\<c-x>\<c-k>\<c-p>\<c-n>", "\<c-x>\<c-k>\<c-p>"],
+      \ 'file'    :  ["\<c-x>\<c-f>\<c-p>\<c-n>", "\<c-x>\<c-f>\<c-p>"],
+      \ 'incl'    :  ["\<c-x>\<c-i>\<c-p>\<c-n>", "\<c-x>\<c-i>\<c-p>"],
+      \ 'keyn'    :  ["\<c-x>\<c-n>\<c-p>\<c-n>", "\<c-x>\<c-n>\<c-p>"],
+      \ 'keyp'    :  ["\<c-x>\<c-p>\<c-n>\<c-p>", "\<c-x>\<c-p>\<c-n>"],
+      \ 'line'    :  ["\<c-x>\<c-l>\<c-n>\<c-p>", "\<c-x>\<c-l>\<c-p>"],
+      \ 'omni'    :  ["\<c-x>\<c-o>\<c-p>\<c-n>", "\<c-x>\<c-o>\<c-p>"],
+      \ 'spel'    :  ["\<c-x>s\<c-p>\<c-n>"     , "\<c-x>s\<c-p>"     ],
+      \ 'tags'    :  ["\<c-x>\<c-]>\<c-n>\<c-p>", "\<c-x>\<c-]>\<c-p>"],
+      \ 'thes'    :  ["\<c-x>\<c-t>\<c-n>\<c-p>", "\<c-x>\<c-t>\<c-p>"],
+      \ 'user'    :  ["\<c-x>\<c-u>\<c-n>\<c-p>", "\<c-x>\<c-u>\<c-p>"]
       \ }, get(g:, 'mucomplete#user_mappings', {}), 'error')
 unlet s:cnp
 let s:compl_methods = []